c7b2d2b7968f04cd0abc422c144bd5443eb790ee
VINÍCIUS MONTEIRO ARAÚJO <22282226@dsn127m1246727.cfp127aluno.local>
2023-08-28 15:57:17 -0300

    Projeto comecado

---
+++ app/src/main/java/br/senai/sp/jandira/screen/login/LoginScreen.kt
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.screen.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen() {
+    
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    
+}
