fc2afe8bc86670e3e35f9e3abf92506a7a8d73dd
VINÍCIUS MONTEIRO ARAÚJO <22282226@dsn127m1246726.cfp127aluno.local>
2023-09-29 16:54:39 -0300

    äää

--- app/src/main/java/br/senai/sp/jandira/cartcomponents/components/Header.kt
+++ app/src/main/java/br/senai/sp/jandira/cartcomponents/components/Header.kt
@@ -31,52 +31,7 @@ import br.senai.sp.jandira.componentes.TextoComTodosAtributos
 
 @Composable
 fun Header() {
-    Column (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ){
 
-            Icone(
-                    painter = painterResource(id = R.drawable.baseline_arrow_back_24),
-                    descricao = "",
-                    corIcone = colorResource(id = R.color.green_41),
-                    modifier = Modifier
-                        .size(25.dp)
-                        .clickable { }
-                )
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-
-
-                TextoComTodosAtributos(
-                    value = stringResource(id = R.string.cart),
-                    corTexto = colorResource(id = R.color.green_41),
-                    fontWeight = FontWeight(600),
-                    fontSize = 20.sp
-                )
-            }
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Row (
-            modifier = Modifier
-                .height(2.dp)
-                .width(400.dp)
-                .background(colorResource(id = R.color.green_41))
-                .clip(RoundedCornerShape(100))
-        ){
-
-        }
-    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/br/senai/sp/jandira/cartcomponents/screen/CartScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/cartcomponents/screen/CartScreen.kt
@@ -1,21 +1,371 @@
 package br.senai.sp.jandira.cartcomponents.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import br.senai.sp.jandira.cartcomponents.components.Form
-import br.senai.sp.jandira.cartcomponents.components.Header
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import br.senai.sp.jandira.R
+import br.senai.sp.jandira.componentes.Icone
+import br.senai.sp.jandira.componentes.Imagem
+import br.senai.sp.jandira.componentes.TextoComTodosAtributos
+import br.senai.sp.jandira.componentes.TextoComTodosAtributosEModifier
 
 @Composable
 fun CartScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
+
+    var unidadeState by remember{
+        mutableStateOf(1)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.white))
+
+    ) {
         Column {
-            Header()
-            Form()
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ){
+
+                    Icone(
+                        painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                        descricao = "",
+                        corIcone = colorResource(id = R.color.green_41),
+                        modifier = Modifier
+                            .size(25.dp)
+                            .clickable { }
+                    )
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+
+
+                        TextoComTodosAtributos(
+                            value = stringResource(id = R.string.cart),
+                            corTexto = colorResource(id = R.color.green_41),
+                            fontWeight = FontWeight(600),
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row (
+                    modifier = Modifier
+                        .height(2.dp)
+                        .width(400.dp)
+                        .background(colorResource(id = R.color.green_41))
+                        .clip(RoundedCornerShape(100))
+                ){
+
+                }
+            }
+
+
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Imagem(
+                        painter = painterResource(id = R.drawable.breadslogo),
+                        descricao = "",
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(60.dp)
+
+                    )
+                    Column(
+                        modifier = Modifier
+                            .absoluteOffset(x = -190.dp)
+
+                    ) {
+                        TextoComTodosAtributos(
+                            value = "Name Restaurant",
+                            corTexto = Color(29, 34, 27),
+                            fontWeight = FontWeight(600),
+                            fontSize = 15.sp
+                        )
+
+                        TextoComTodosAtributosEModifier(
+                            value = stringResource(id = R.string.menu),
+                            corTexto = colorResource(id = R.color.green_41),
+                            fontWeight = FontWeight(300),
+                            fontSize = 10.sp,
+                            modifier = Modifier
+                                .clickable { }
+                        )
+                    }
+                }
+
+                LazyColumn(){
+                    items(7){
+                        Card(
+                            modifier = Modifier
+                                .height(130.dp)
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                                .shadow(
+                                    elevation = 2.dp,
+                                    spotColor = Color(0xFF000000),
+                                    ambientColor = Color(0xFF000000),
+                                    shape = RoundedCornerShape(32.dp)
+                                ),
+                            colors = CardDefaults.cardColors(
+                                colorResource(id = R.color.card_cart_background)
+                            ),
+                            shape = RoundedCornerShape(10.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                            ) {
+                                Row (
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(70.dp)
+                                        .padding(5.dp),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ){
+                                    Imagem(
+                                        painter = painterResource(id = R.drawable.coxinha),
+                                        descricao = "",
+                                        modifier = Modifier
+                                            .size(60.dp)
+                                            .clip(RoundedCornerShape(100))
+                                    )
+                                    Column (modifier = Modifier.absoluteOffset(x = -80.dp)){
+                                        TextoComTodosAtributos(
+                                            value = "Coxinha",
+                                            corTexto = colorResource(id = R.color.black),
+                                            fontWeight = FontWeight(400),
+                                            fontSize = 18.sp
+                                        )
+                                        TextoComTodosAtributos(
+                                            value = "3 unidades - R$4,50",
+                                            corTexto = colorResource(id = R.color.black),
+                                            fontWeight = FontWeight(300),
+                                            fontSize = 12.sp
+                                        )
+                                    }
+                                    
+                                    Icone(
+                                        painter = painterResource(id = R.drawable.outline_delete_24),
+                                        descricao = "",
+                                        corIcone = colorResource(id = R.color.black),
+                                        modifier = Modifier.size(25.dp)
+                                    )
+
+                                }
+                                Row (
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(3.dp)
+                                    ,
+                                    horizontalArrangement = Arrangement.End
+                                ){
+                                    Row (
+                                        modifier = Modifier
+                                            .height(30.dp)
+                                            .width(75.dp)
+                                            .background(
+                                                colorResource(id = R.color.greenTextField),
+                                                shape = RoundedCornerShape(100)
+                                            )
+                                            .padding(5.dp),
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        horizontalArrangement = Arrangement.SpaceBetween
+
+                                    ){
+                                        Icone(
+                                            painter = painterResource(id = R.drawable.baseline_remove_24),
+                                            descricao = "",
+                                            corIcone = colorResource(R.color.white),
+                                            modifier = Modifier
+                                                .clickable { unidadeState-- }
+                                                .size(17.dp)
+                                        )
+
+                                        TextoComTodosAtributos(
+                                            value = "${unidadeState}",
+                                            corTexto = colorResource(id = R.color.white),
+                                            fontWeight = null,
+                                            fontSize = 15.sp
+                                        )
+                                        Icone(
+                                            painter = painterResource(id = R.drawable.baseline_add_24),
+                                            descricao = "",
+                                            corIcone = colorResource(id = R.color.white),
+                                            modifier = Modifier
+                                                .clickable { unidadeState++ }
+                                                .size(17.dp)
+                                        )
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .absoluteOffset(y = 660.dp)
+                .clip(RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp))
+                .background(Color(230, 233, 240)),
+
+        ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row (modifier = Modifier.fillMaxWidth()){
+                    TextoComTodosAtributos(
+                        value = "Resumo de valores",
+                        corTexto = colorResource(id = R.color.green_41),
+                        fontWeight = FontWeight(600),
+                        fontSize = 15.sp
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Column(
+                    modifier = Modifier.height(60.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+
+                ) {
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        TextoComTodosAtributos(
+                            value = "SubTotal",
+                            corTexto = colorResource(id = R.color.gray),
+                            fontWeight = FontWeight(300),
+                            fontSize = 12.sp
+                        )
+
+                        TextoComTodosAtributos(
+                            value = "R$ 30,00",
+                            corTexto = colorResource(id = R.color.black),
+                            fontWeight = FontWeight(600),
+                            fontSize = 12.sp
+                        )
+
+                    }
+                    Row (modifier = Modifier
+                        .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween){
+
+                        TextoComTodosAtributos(
+                            value = "Taxa de Entrega",
+                            corTexto = colorResource(id = R.color.gray),
+                            fontWeight = FontWeight(300),
+                            fontSize = 12.sp
+                        )
+
+                        TextoComTodosAtributos(
+                            value = "Gratis",
+                            corTexto = colorResource(id = R.color.green_41),
+                            fontWeight = FontWeight(600),
+                            fontSize = 12.sp
+                        )
+
+                    }
+                    Row (modifier = Modifier
+                        .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween){
+
+                        TextoComTodosAtributos(
+                            value = "Total",
+                            corTexto = colorResource(id = R.color.gray),
+                            fontWeight = FontWeight(300),
+                            fontSize = 12.sp
+                        )
+
+                        TextoComTodosAtributos(
+                            value = "R$ 30,00",
+                            corTexto = colorResource(id = R.color.green_41),
+                            fontWeight = FontWeight(600),
+                            fontSize = 12.sp
+                        )
+
+                    }
+                }
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        colorResource(id = R.color.greenTextField)
+                    )
+                ) {
+                    TextoComTodosAtributos(
+                        value = "Continuar",
+                        corTexto = colorResource(id = R.color.white),
+                        fontWeight = null,
+                        fontSize = 15.sp
+                    )
+                    
+                }
+            }
+
         }
     }
 }
@@ -24,4 +374,7 @@ fun CartScreen() {
 @Composable
 fun CartScreenPreview() {
 
+    CartScreen()
+
 }
+
